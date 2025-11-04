811ac141d3261d8a631fbfe57f4b7bf07f2fd1a9
Adan Shahid <adanshahid.student@gmail.com>
2025-11-04 13:58:51 +0500

    Day_5 Completed

--- Day_5.py
+++ Day_5.py
@@ -1,18 +1,23 @@
 # REMOVE DUPLICATES FROM THE SORTED ARRAY. IN_PLACE.
 
-arr = [0, 0, 1, 1, 1, 1, 2, 3, 4]
-count = 1
-total_count = 1
-ans = 0
-i = 1
-while i < len(arr):
-    if arr[i-1] == arr[i]:
-        count += 1
-        total_count += count
-        if count == 2:
-            i = i + 1
-            count = 1
-    elif arr[i-1] != arr[i]:
-        i = i + 1
-        total_count = total_count + count
-print(total_count)
+num = [0,0,1,1,1,1,2,3,3]
+
+i = 0
+for n in num:
+    if i < 2 or n > num[i-2]:
+        num[i] = n
+        i += 1
+print(i, num)
+
+# l, r = 0, 0
+# while r < len(num):
+#     count = 1
+#     while r + 1 < len(num) and num[r] == num[r + 1]:
+#         r += 1
+#         count += 1
+    
+#     for i in range(min(2, count)):
+#         num[l] = num[r]
+#         l += 1
+#     r += 1
+# print(l, num)
