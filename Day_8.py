d8551273d8543fda138e6957ae725b8ee3570534
Adan Shahid <adanshahid.student@gmail.com>
2025-12-08 10:10:00 +0500

    Day_8 completed

--- Day_8.py
+++ Day_8.py
@@ -1,28 +1,17 @@
 # Buy and Sell Stock
 prices = [7,1,5,3,6,4]
-max_price = max(prices)
-min_price = min(prices)
-buy_price = 0
-sell_price = 0
-max_profit = 0
-if prices == sorted(prices, reverse=True):   
-    print(max_profit)
-else:
-    i = 0
-    while i < len(prices):
-        if prices[i] ==  min_price:
-            buy_price = prices[i]
-        i = i + 1
-        
-    j = 1
-    while j < len(prices):
-        if prices[j] ==  max_price:
-            sell_price = prices[j]
-        j = j + 1
-        
-    print(buy_price)
-    print(sell_price)
-    max_profit = sell_price - buy_price
-    print(max_profit)
+l,r = 0, 1 #left = buy. right = sell
+maxProfit = 0
+
+while r < len(prices):
+    if prices[l] < prices[r]:
+        profit = prices[r] - prices[l]
+        maxProfit = max(maxProfit, profit)
+    else:
+         l = r
+    r += 1
+
+print(maxProfit)
+
 
 
